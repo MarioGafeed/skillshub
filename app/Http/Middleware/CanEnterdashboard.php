27f8a5b3be0337caaba1a1537f7553a3d6fed84b
MarioGafeed <jesushostingme@gmail.com>
2021-08-22 02:00:30 +0200

    fix CanEnterDashboard
    fix CanEnterDashboard

--- app/Http/Middleware/CanEnterdashboard.php
+++ app/Http/Middleware/CanEnterdashboard.php
@@ -1,10 +1,16 @@
 <?php
 
+// namespace App\Http\Middleware;
+//
+// use Closure;
+// use Illuminate\Support\Facades\Auth;
+// use Illuminate\Http\Request;
+
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CanEnterDashboard
 {
