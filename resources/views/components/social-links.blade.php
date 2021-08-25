e7dc1de9d4cbef5972a05bff598cb8d10da3a606
MarioGafeed <jesushostingme@gmail.com>
2021-08-25 09:56:48 +0200

    fix
    fix

--- resources/views/components/social-links.blade.php
+++ resources/views/components/social-links.blade.php
@@ -1,6 +1,6 @@
 <ul class="footer-social">
 
-@if($setting->fb != null)
+<!-- @if($setting->fb != null)
 <li><a href="{{ $setting->fb }}" target="_blank" class="facebook"><i class="fa fa-facebook"></i></a></li>
 @endif
 @if($setting->tw != null)
@@ -8,7 +8,7 @@
 @endif
 @if($setting->insta != null)
 <li><a href="{{ $setting->insta }}" target="_blank" class="instagram"><i class="fa fa-instagram"></i></a></li>
-@endif
+@endif -->
 
 
 </ul>
