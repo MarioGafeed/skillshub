734a43322e19b097321c70862ced35915a1108ee
MarioGafeed <jesushostingme@gmail.com>
2024-02-09 19:04:05 +0200

    active exam

--- app/Http/Controllers/Web/HomeController.php
+++ app/Http/Controllers/Web/HomeController.php
@@ -10,7 +10,7 @@ class HomeController extends Controller
     public function index()
     {
       $data['cats']  = Cat::select('id', 'name')->get();
-      $data['exams'] = Exam::OrderBy('id', 'DESC')->paginate(8);
+      $data['exams'] = Exam::where('active', '1')->OrderBy('id', 'DESC')->paginate(8);
       return view('web.home.index')->with($data);
     }
 }
